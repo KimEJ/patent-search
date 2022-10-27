9560e1b0fc3aafc89671133747ad383cc609eebc
Kim Eo Jin <ujkim@interpass.kr>
2022-10-27 17:20:24 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'patent-search',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
